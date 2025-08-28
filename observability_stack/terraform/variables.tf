aa71787366b750a069def7da6a8310fd71261700
ramisettybhargavi <60207218+ramisettybhargavi@users.noreply.github.com>
2025-08-28 10:56:09 +0530

    Update variables.tf

--- observability_stack/terraform/variables.tf
+++ observability_stack/terraform/variables.tf
@@ -5,49 +5,49 @@ variable "aws_region" {
 }
 
 variable "cluster_name" {
-  description = "EKS cluster name"
+  description = "Name of the EKS cluster"
   type        = string
   default     = "observability-cluster"
 }
 
 variable "cluster_version" {
-  description = "Kubernetes version for EKS"
+  description = "Kubernetes version to use for the EKS cluster"
   type        = string
   default     = "1.29"
 }
 
 variable "vpc_cidr" {
-  description = "VPC CIDR block"
+  description = "CIDR block for VPC"
   type        = string
   default     = "10.0.0.0/16"
 }
 
 variable "environment" {
-  description = "Deployment environment"
+  description = "Environment name"
   type        = string
   default     = "production"
 }
 
 variable "node_instance_types" {
-  description = "EKS node instance types"
+  description = "Instance types for EKS node groups"
   type        = list(string)
   default     = ["m5.large", "m5.xlarge"]
 }
 
 variable "node_desired_capacity" {
-  description = "Desired EKS node count"
+  description = "Desired capacity for EKS node group"
   type        = number
   default     = 3
 }
 
 variable "node_max_capacity" {
-  description = "Max EKS node count"
+  description = "Maximum capacity for EKS node group"
   type        = number
-  default     = 3
+  default     = 10
 }
 
 variable "node_min_capacity" {
-  description = "Min EKS node count"
+  description = "Minimum capacity for EKS node group"
   type        = number
   default     = 1
 }
