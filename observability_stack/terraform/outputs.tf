551cded0d5dc169aa798c583a48998332b9f9d08
ramisettybhargavi <60207218+ramisettybhargavi@users.noreply.github.com>
2025-08-28 10:56:27 +0530

    Update outputs.tf

--- observability_stack/terraform/outputs.tf
+++ observability_stack/terraform/outputs.tf
@@ -9,7 +9,7 @@ output "cluster_arn" {
 }
 
 output "cluster_endpoint" {
-  description = "EKS API endpoint"
+  description = "EKS cluster API server endpoint"
   value       = module.eks.cluster_endpoint
 }
 
@@ -18,17 +18,32 @@ output "cluster_name" {
   value       = module.eks.cluster_name
 }
 
+output "cluster_security_group_id" {
+  description = "Security group ids attached to the cluster control plane"
+  value       = module.eks.cluster_security_group_id
+}
+
 output "vpc_id" {
   description = "VPC ID"
   value       = module.vpc.vpc_id
 }
 
 output "private_subnets" {
-  description = "Private subnet IDs"
+  description = "List of IDs of private subnets"
   value       = module.vpc.private_subnets
 }
 
 output "public_subnets" {
-  description = "Public subnet IDs"
+  description = "List of IDs of public subnets"
   value       = module.vpc.public_subnets
+}
+
+output "node_group_security_group_id" {
+  description = "Security group ID for EKS node group"
+  value       = module.security.node_group_security_group_id
+}
+
+output "observability_services_security_group_id" {
+  description = "Security group ID for observability services"
+  value       = module.security.observability_services_security_group_id
 }
