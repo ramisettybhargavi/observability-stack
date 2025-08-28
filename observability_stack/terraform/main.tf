da18a950eff7d6f3952a557738cbe3b84f0e90ea
ramisettybhargavi <60207218+ramisettybhargavi@users.noreply.github.com>
2025-08-28 15:36:18 +0530

    Update main.tf

--- observability_stack/terraform/main.tf
+++ observability_stack/terraform/main.tf
@@ -18,7 +18,27 @@ terraform {
 provider "aws" {
   region = var.region
 }
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
+}
 
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    }
+  }
+}
 module "vpc" {
   source = "./modules/VPC"
 
@@ -38,3 +58,4 @@ module "eks" {
   subnet_ids      = module.vpc.private_subnet_ids
   node_groups     = var.node_groups
 }
+
