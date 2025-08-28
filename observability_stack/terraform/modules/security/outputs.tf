656d0ecf8cbb726292eb1efc7d997b24947c13e2
ramisettybhargavi <60207218+ramisettybhargavi@users.noreply.github.com>
2025-08-28 15:23:34 +0530

    Delete observability_stack/terraform/modules/security directory

--- observability_stack/terraform/modules/security/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "node_group_security_group_id" {
-  description = "Security group ID for EKS node group"
-  value       = aws_security_group.node_group_one.id
-}
-
-output "observability_services_security_group_id" {
-  description = "Security group ID for observability services"
-  value       = aws_security_group.observability_services.id
-}
