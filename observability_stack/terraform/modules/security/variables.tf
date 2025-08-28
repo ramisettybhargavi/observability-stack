21798f7aa5a8b954ce584ff6e2cc2cc63e7b0c85
ramisettybhargavi <60207218+ramisettybhargavi@users.noreply.github.com>
2025-08-28 10:51:29 +0530

    Create variables.tf

---
+++ observability_stack/terraform/modules/security/variables.tf
@@ -0,0 +1,14 @@
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+}
