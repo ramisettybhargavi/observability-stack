ea137daa657fdcb8a71d43aa6a09cd38b7332dbc
ramisettybhargavi <60207218+ramisettybhargavi@users.noreply.github.com>
2025-08-28 10:57:09 +0530

    Update provider.tf

--- observability_stack/terraform/provider.tf
+++ observability_stack/terraform/provider.tf
@@ -1,3 +1,32 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.20"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.10"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1"
+    }
+  }
+
+  # Uncomment and configure for remote state
+  # backend "s3" {
+  #   bucket = "your-terraform-state-bucket"
+  #   key    = "observability/terraform.tfstate"
+  #   region = "us-west-2"
+  # }
+}
+
 provider "aws" {
   region = var.aws_region
 }
@@ -5,7 +34,7 @@ provider "aws" {
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-
+  
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
@@ -17,7 +46,7 @@ provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-
+    
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
