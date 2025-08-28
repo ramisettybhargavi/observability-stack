788039a15a416f225c0603305b6a1fcbe4ae867a
ramisettybhargavi <60207218+ramisettybhargavi@users.noreply.github.com>
2025-08-28 10:54:21 +0530

    Create outputs.tf

---
+++ observability_stack/terraform/modules/eks/outputs.tf
@@ -0,0 +1,29 @@
+output "cluster_id" {
+  description = "EKS cluster ID"
+  value       = module.eks.cluster_id
+}
+
+output "cluster_arn" {
+  description = "EKS cluster ARN"
+  value       = module.eks.cluster_arn
+}
+
+output "cluster_endpoint" {
+  description = "EKS cluster API server endpoint"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_name" {
+  description = "EKS cluster name"
+  value       = module.eks.cluster_name
+}
+
+output "cluster_security_group_id" {
+  description = "Security group ids attached to the cluster control plane"
+  value       = module.eks.cluster_security_group_id
+}
+
+output "cluster_certificate_authority_data" {
+  description = "Base64 encoded certificate data required to communicate with the cluster"
+  value       = module.eks.cluster_certificate_authority_data
+}
